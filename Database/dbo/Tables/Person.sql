14b406c5fe8d29dd7e54462d0a2a91c91b931782
Chelsey Hecker <cnhecker@mail.fhsu.edu>
2025-06-16 20:24:04 -0400

    added react router

--- Database/dbo/Tables/Person.sql
+++ Database/dbo/Tables/Person.sql
@@ -2,5 +2,6 @@
 (
 	[P_Id] INT IDENTITY (1,1) NOT NULL PRIMARY KEY, 
     [P_FirstName] VARCHAR(50) NOT NULL, 
-    [P_LastName] VARCHAR(50) NOT NULL
+    [P_LastName] VARCHAR(50) NOT NULL, 
+    [P_DateOfBirth] DATE NULL
 )
